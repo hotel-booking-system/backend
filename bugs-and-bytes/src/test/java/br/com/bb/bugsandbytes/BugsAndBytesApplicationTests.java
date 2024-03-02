a955e2501d934787cb20f0894a3eb2681f7b28be
Juliane Maran <julianemaran@gmail.com>
2024-03-02 13:58:08 -0300

    initial project

---
+++ bugs-and-bytes/src/test/java/br/com/bb/bugsandbytes/BugsAndBytesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bb.bugsandbytes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugsAndBytesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
