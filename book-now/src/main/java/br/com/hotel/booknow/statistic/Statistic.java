62cce9fc04a64bb1754a4311f6d8e1ced513e772
Juliane Maran <julianemaran@gmail.com>
2024-03-19 07:56:53 -0300

    feat: add statistic

--- book-now/src/main/java/br/com/hotel/booknow/statistic/Statistic.java
+++ book-now/src/main/java/br/com/hotel/booknow/statistic/Statistic.java
@@ -1,2 +1,29 @@
-package br.com.hotel.booknow.statistic;public class Statistic {
+package br.com.hotel.booknow.statistic;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Statistic {
+
+	// soma total
+	private BigDecimal sum;
+
+	// média dos valores
+	private BigDecimal avg;
+
+	// maior valor
+	private BigDecimal max;
+
+	// menor valor
+	private BigDecimal min;
+
+	// número total
+	private Long count;
+
 }
--- book-now/src/main/java/br/com/hotel/booknow/statistic/StatisticService.java
+++ book-now/src/main/java/br/com/hotel/booknow/statistic/StatisticService.java
@@ -1,2 +1,24 @@
-package br.com.hotel.booknow.statistic;public class StatisticService {
+package br.com.hotel.booknow.statistic;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class StatisticService {
+
+	public Statistic create() {
+
+		return Statistic.builder()
+				.count(1L)
+				.avg(BigDecimal.valueOf(10))
+				.min(BigDecimal.valueOf(10))
+				.max(BigDecimal.valueOf(10))
+				.sum(BigDecimal.valueOf(10))
+				.build();
+
+	}
+
+	// https://mari-azevedo.medium.com/construindo-uma-api-restful-com-java-e-spring-framework-46b74371d107
+
 }
