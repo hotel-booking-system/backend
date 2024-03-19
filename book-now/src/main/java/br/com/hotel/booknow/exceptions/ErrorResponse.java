81e8f24f7f47deb11d92a5f272f9246b0916f345
Juliane Maran <julianemaran@gmail.com>
2024-03-19 07:55:17 -0300

    feat: add fluxo de erro personalizado para server error

--- book-now/src/main/java/br/com/hotel/booknow/exceptions/ErrorResponse.java
+++ book-now/src/main/java/br/com/hotel/booknow/exceptions/ErrorResponse.java
@@ -1,2 +1,17 @@
-package br.com.hotel.booknow.exceptions;public class ErrorResponse {
+package br.com.hotel.booknow.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Setter
+@Getter
+@AllArgsConstructor
+public class ErrorResponse {
+
+	private HttpStatus status;
+	private String error;
+	private String message;
+
 }
